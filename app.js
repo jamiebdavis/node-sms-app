048a9e151078ebea993a146e012ce42480dcde58
Jamie Davis <jamied1985@gmail.com>
2018-12-10 18:04:56 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const Nexmo = require("nexmo");
+const socketio = require("socket.io");
+
+//Init App
+
+const app = express();
+
+//template engine setup
+
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
+
+//public folder setup
+
+app.use(express.static(__dirname + "/public"));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("listening on port 3000");
+});
